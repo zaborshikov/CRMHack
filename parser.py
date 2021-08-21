424264c16da02a9ba3ce020b7da79b306058bd76
Uknown-creator <kuskov779@gmail.com>
2021-08-21 18:34:12 +0300

    Update parser.py & ai.py

--- parser.py
+++ parser.py
@@ -1,11 +1,15 @@
 from logging import error
 import vk_api
 import creds
-import analyze
+import ai
+
+
 def auth_handler():
     #2-factor verification
     key = input('Enter authentication code: ')
     return key, True
+
+
 def main():
     vk_session = vk_api.VkApi(creds.login, creds.password, auth_handler=auth_handler)
     vk_session.auth()
@@ -14,7 +18,7 @@ def main():
     postidlist = vk.wall.get(owner_id=analyze_id, count=100, offset=0) 
     n = 0
     m = 0
-    comments = []
+    commentsEmoji = []
     posts = []
     while n < 101:
         m = 0
@@ -24,7 +28,7 @@ def main():
             while m < 101:
                 try:
                     textComment = response['items'][m]['text']
-                    comments.insert(m, textComment)
+                    commentsEmoji.insert(m, textComment)
                     m += 1
                 except IndexError:
                     break
@@ -32,6 +36,24 @@ def main():
             n += 1
         except IndexError:
             break
-    return(comments, posts)
+
+    
+    # comments = [word if word != "👍" else 'Класс!' for word in commentsEmoji]
+    # comments = [word if word != "🙃" else 'Прикольно' for word in commentsEmoji]
+    # comments = [word if word != "🤨" else 'Что?' for word in commentsEmoji]
+    # comments = [word if word != "👌" else 'Хорошо!' for word in commentsEmoji]
+    # comments = [word if word != "🤷" else 'Не знаю...' for word in commentsEmoji]
+    # comments = [word if word != "🤞" else 'Будем надеяться' for word in commentsEmoji]
+    # comments = [word if word != "🔥" else 'Супер!' for word in commentsEmoji]
+
+    # print(comments)    
+    for i in commentsEmoji:
+        return(ai.analyze([i]))
+    # print(ai.analyze(comments))
+    # return(comments, posts)
+    # return(comments, posts)
+
+
+
 if __name__ == '__main__':
     print(main())
