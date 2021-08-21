14b468515da18758966ea2c349ec5c0f860216d8
Uknown-creator <kuskov779@gmail.com>
2021-08-21 15:11:49 +0300

    Parser MVP

--- parser.py
+++ parser.py
@@ -12,20 +12,29 @@ def main():
     analyze_id = str(-190900168)
     postidlist = vk.wall.get(owner_id=analyze_id, count=100, offset=0) 
     n = 0
+    m = 0
     comments = []
+    posts = []
     while n < 5:
+        m = 0
         try:
             postid = str(postidlist['items'][n]['id'])
             print(postid)
             response = vk.wall.getComments(owner_id=analyze_id, post_id=postid, count=100, sort='desc', offset=0)
-            try:
-                textComment = response['items'][0]['text']
-                print(textComment)
-            except IndexError:
-                break
+            while m < 10:
+                try:
+                    textComment = response['items'][m]['text']
+                    print(textComment)
+                    comments.insert(m, textComment)
+                    m += 1
+                except IndexError:
+                    break
             print(postid)
+            posts.insert(n, postid)
             n += 1
         except IndexError:
             break
+    print(comments)
+    print(posts)
 if __name__ == '__main__':
     main()
