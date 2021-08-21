1049dd12fad4b2c9e83e9964145e859234deb52b
kniazevgeny <kniazev-evg@ya.ru>
2021-08-22 01:22:03 +0300

    feat: LineChart.vue, upd: logo

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,21 +1,19 @@
 import store from '@/store'
 import axios from 'axios'
 import { User } from '@/models/User'
-import { namespace } from 'vuex-class'
-
-// const AppStore = namespace('AppStore')
-// let AppStore.State api_base!: string;
-
-//TODO: test this
 
 export async function create(link: string) {
   // console.log(store.state.AppStore.api_base);
   return (
     await axios.post(
-      `${store.state.AppStore.api_base}/create`,
+      `${getApiBase()}/create`,
       {"vk_link": link},
     )
   ).data
+}
+
+function getApiBase() {
+  return (store as any).state.AppStore.api_base;
 }
 
 function getHeaders(user: User) {
--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,16 +1,16 @@
 <template lang="pug">
 .v-container.pa-4.mt-12
   // Main content
-  v-layout()
+  v-layout
     v-flex(xs1, md2)
     v-flex(xs10, md8)
       span._headline.pt-4 Дэшборд
-      v-layout(v-if="typeof $store.state.AppStore.group_id !== 'undefined'")
+      v-layout(v-if='typeof $store.state.AppStore.group_id !== "undefined"')
         span yeah
+        LineChart(:chartdata='chartData', :options='chartOptions')
       v-layout(v-else) 
-        span Что-то пошло не так. Попробуйте начать сначала. 
+        span Что-то пошло не так. Попробуйте начать сначала.
     v-flex(xs1, md2)
-
 </template>
 
 <script lang="ts">
@@ -19,49 +19,60 @@ import { create } from '@/utils/api'
 import Component from 'vue-class-component'
 import { i18n } from '@/plugins/i18n'
 import { namespace } from 'vuex-class'
-import { User } from '@/models/User'
+import { Line } from 'vue-chartjs'
+import LineChart from '@/components/LineChart.vue'
 
 const AppStore = namespace('AppStore')
 const SnackbarStore = namespace('SnackbarStore')
 
+Vue.component('LineChart', LineChart)
+
 @Component
 export default class Home extends Vue {
-  @AppStore.Mutation setUser!: (user: User) => void
   @SnackbarStore.Mutation setSnackbarError!: (error: string) => void
-  link: string = "";
-  loading: boolean = false;
-  btn_icon: string = "mdi-arrow-right";
-  color: string = "black";
 
-  async sendCompanyLink() {
-    this.loading = true;
-    create(this.link).then(() => {
-      this.loading = false;
-      this.color = "green";
-      this.btn_icon = "mdi-done";
-    })
-    .catch(e => {
-      console.log(e);
-      this.loading = false;
-      this.btn_icon = "mdi-close";
-      this.color = "red";
-      this.delay(2000).then(() => {
-        this.btn_icon = "mdi-arrow-right";
-        this.color = "black";
-      })
-    })
+  gradient: any = "red"
+  gradient2: any = "blue"
+
+
+
+  chartData = {
+    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+    datasets: [
+      {
+        label: 'Data One',
+        borderColor: '#FC2525',
+        pointBackgroundColor: 'white',
+        borderWidth: 1,
+        pointBorderColor: 'white',
+        backgroundColor: this.gradient,
+        data: [40, 39, 10, 40, 39, 80, 40],
+      },
+      {
+        label: 'Data Two',
+        borderColor: '#05CBE1',
+        pointBackgroundColor: 'white',
+        pointBorderColor: 'white',
+        borderWidth: 1,
+        backgroundColor: this.gradient2,
+        data: [60, 55, 32, 10, 2, 12, 53],
+      },
+    ],
   }
-  
+  chartOptions = {
+    responsive: true,
+    maintainAspectRatio: false,
+  }
+
   async delay(ms: number) {
-    return new Promise( resolve => setTimeout(resolve, ms) );
+    return new Promise((resolve) => setTimeout(resolve, ms))
   }
-
 }
 </script>
 
 <style>
 ._headline {
-  font-size: 3em!important;
+  font-size: 3em !important;
   font-family: 'Inter';
   font-weight: 800;
   line-height: 85%;
