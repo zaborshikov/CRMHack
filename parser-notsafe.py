1d717ab1f8520f48b08e0016173850f295c66fca
Uknown-creator <kuskov779@gmail.com>
2021-08-21 12:41:27 +0300

    Update parsing script; Exception and fuctions was added

--- parser-notsafe.py
+++ parser-notsafe.py
@@ -1,18 +1,24 @@
 from logging import error
 import vk_api
 import creds
-vk_session = vk_api.VkApi(creds.login, creds.password)
-try:
-    vk_session.authorization()
-except vk_api.AuthorizationError as error_msg:
-    print(error_msg)
+def auth_handler():
+    #2-factor verification
+    key = input('Enter authentication code: ')
+    return key, True
+def main():
+    vk_session = vk_api.VkApi(creds.login, creds.password, auth_handler=auth_handler)
+    vk_session.auth()
+    vk = vk_session.get_api()
 
-vk = vk_session.get_api()
-
-analyze_id = str(-29534144)
-postidlist = vk.wall.get(owner_id=analyze_id, count=1, offset=0)
-a = str(postidlist['items'][0]['id'])
-response = vk.wall.getComments(owner_id=analyze_id, post_id=a, count=1, sort='desc', offset=0)
-b = response['items'][0]['text']
-print(a)
-print(b)
+    analyze_id = str(-190900168)
+    postidlist = vk.wall.get(owner_id=analyze_id, count=1, offset=0)
+    idOfPersonComment = str(postidlist['items'][0]['id'])
+    response = vk.wall.getComments(owner_id=analyze_id, post_id=idOfPersonComment, count=1, sort='desc', offset=0)
+    try:
+        textComment = response['items'][0]['text']
+        print(idOfPersonComment)
+        print(textComment)
+    except IndexError:
+        print('вы ненужное сообщество. живите с этим.')
+if __name__ == '__main__':
+    main()
