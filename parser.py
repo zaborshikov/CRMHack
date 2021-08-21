24b76c0830e9f0c0c079ecae5fa4ee76d08b4c01
Uknown-creator <kuskov779@gmail.com>
2021-08-21 14:48:42 +0300

    Delete comments

--- parser.py
+++ parser.py
@@ -10,50 +10,22 @@ def main():
     vk_session.auth()
     vk = vk_session.get_api()
     analyze_id = str(-190900168)
-    # n = 0
-    # m = 0
-    # for i in postsGroup:
-    # while n < 10:
-    #     idOfPost = str(['items'][0]['id'])
-    #     while m < 10:
-    #         response = vk.wall.getComments(owner_id=analyze_id, post_id=idOfPost, count=m, sort='desc', offset=0)
-    #         try:
-    #             textComment = response['items'][m]['text']
-    #             print(textComment)
-    #             print(idOfPost)
-    #         except IndexError:
-    #             pass
-    #         m += 1
-    #     n += 1
-
-
-    # postidlist = vk.wall.get(owner_id=analyze_id, count=1, offset=0)
-    # idOfPost = str(postidlist['items'][0]['id'])
-    # response = vk.wall.getComments(owner_id=analyze_id, post_id=idOfPost, count=1, sort='desc', offset=0)
-    # idOfPersonComment = str(idOfPost['items'][0]['id'])
-
-    postidlist = vk.wall.get(owner_id=analyze_id, count=100, offset=0) #получаем последний пост со стены
+    postidlist = vk.wall.get(owner_id=analyze_id, count=100, offset=0) 
     n = 0
-    m = 0
-    while n < 101:
+    comments = []
+    while n < 5:
         try:
             postid = str(postidlist['items'][n]['id'])
             print(postid)
-            response = vk.wall.getComments(owner_id=analyze_id, post_id=postid, count=100, sort='desc', offset=0) #Получаем последний комментарий в последнем посте со стены
-            while m < 101:
-                try:
-                    textComment = response['items'][m]['text']
-                    print(textComment)
-                    m += 1
-                except IndexError:
-                    break
+            response = vk.wall.getComments(owner_id=analyze_id, post_id=postid, count=100, sort='desc', offset=0)
+            try:
+                textComment = response['items'][0]['text']
+                print(textComment)
+            except IndexError:
+                break
             print(postid)
             n += 1
         except IndexError:
             break
-    # postid = str(postidlist['items'][1]['id']) #получаем id поста в виде цифры и записываем
-    # print(postsGroup)\
-    # except IndexError:
-    #     print('вы ненужное сообщество. живите с этим.')
 if __name__ == '__main__':
     main()
